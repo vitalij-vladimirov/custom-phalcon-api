3d01b8c77c7e123d75c72adf8983d63e4485f1a9
Vitalij Vladimirov <vitalij.vladimirov@gmail.com>
2020-03-14 20:28:20 +0200

    Base MVC structure.

--- app/public/index.php
+++ app/public/index.php
@@ -1,2 +1,60 @@
 <?php
-phpinfo();
+declare(strict_types=1);
+
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Mvc\Micro;
+
+
+define('BASE_PATH', dirname(__DIR__));
+define('APP_PATH', BASE_PATH);
+
+try {
+    /**
+     * The FactoryDefault Dependency Injector automatically registers the services that
+     * provide a full stack framework. These default services can be overidden with custom ones.
+     */
+    $di = new FactoryDefault();
+
+    /**
+     * Include Services
+     */
+    include APP_PATH . '/config/services.php';
+
+    /**
+     * Get config service for use in inline setup below
+     */
+    $config = $di->getConfig();
+
+    /**
+     * Include Autoloader
+     */
+    include APP_PATH . '/config/loader.php';
+
+    /**
+     * Starting the application
+     * Assign service locator to the application
+     */
+    $app = new Micro($di);
+
+    /**
+     * Include Application
+     */
+    include APP_PATH . '/app.php';
+
+    /**
+     * Include vendors
+     */
+    include APP_PATH . '/vendor/autoload.php';
+
+    /**
+     * Handle the request
+     */
+    $app->handle($_SERVER['REQUEST_URI']);
+} catch (\Exception $e) {
+      echo $e->getMessage() . '<br>';
+      echo '<pre>' . $e->getTraceAsString() . '</pre>';
+}
